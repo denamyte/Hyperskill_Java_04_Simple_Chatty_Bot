a3cf89b5f6bcfc022074c81e3613fcff25c6d4bc
denamyte <denamyte@gmail.com>
2020-11-14 15:15:15 +0200

    2020-11-14: Stage 2/5

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -1,7 +1,17 @@
 package bot;
 
+import java.util.Scanner;
+
 public class SimpleBot {
     public static void main(String[] args) {
-        System.out.println("Hello! My name is Aid.\nI was created in 2020.");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello! My name is Aid.");
+        System.out.println("I was created in 2018.");
+        System.out.println("Please, remind me your name.");
+
+        String name = scanner.nextLine();
+
+        System.out.printf("What a great name you have, %s!", name);
     }
 }
